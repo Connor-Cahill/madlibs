64640ae8f1fa2b661978f4e3886df6b06ee8a89b
Connor Cahill <connorcahill913@gmail.com>
2018-10-29 16:37:27 -0700

    seperated random story selector to its own function

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,5 @@
-# Need to find set of 3 stories
-#store 3 stories in a list
+# Need to find set of 2-3 stories
+#store stories in a list
 #create function that gets inputs from user (NEED: Verb, Noun, Adjective, etc.)
 #choose random index from list to pick story
 ##Format the list to inject the user_inputted values into the necessary spots
@@ -8,12 +8,18 @@ import random # imported for random.choice()
 
 stories = ["Last summer, my mom and dad took me and {person} on a trip to {location}. The weather there is very {adj}! Northern {location} has many {plural_noun}, and they make {adj2} {plural_noun2} there. Many people also go to {location} to {verb} or see {noun1}. The people that live there love to eat {food} and are very proud of their big {noun2}.",
 "Hi! This is {name}, speaking to you from the broadcasting {noun} at the {adj} forum. In case you {verb} in late, the score between the Los Angeles {noun2} and the Boston {noun3} is squeker, 141 to {number}. This has been the most {adj2} game these {noun4} eyes have seen in years. First, one team scores a {noun5}, then the other team comes right back. What an interesting {noun6} to watch!",]
+story = ''
+def selectStory(list):
+    """Randomly selects one of the stories from the stories list and returns it as a variable """
+    story = random.choice(list)
+    return story
 
 
 def madlibs():
-    random.choice(stories)
+    """Depending on story returned by selectStory() will ask user series of questions to fill out story """
     print('Please answer the following to fill in blanks of the mad libs story!')
-    if stories[0]:
+    selectStory(stories)
+    if story == stories[0]:
         person = str(input("Enter a person: "))
         location = str(input('Enter a location: '))
         adj = str(input('Enter in adjective: '))
@@ -38,6 +44,7 @@ def madlibs():
         noun4 = str(input('Enter another noun: '))
         noun5 = str(input('Enter another noun: '))
         noun6 = str(input('Enter another noun: '))
-        return print(stories[1].form(name = name, noun = noun, adj = adj, verb = verb, noun2 = noun2, noun3 = noun3,
+        return print(stories[1].format(name = name, noun = noun, adj = adj, verb = verb, noun2 = noun2, noun3 = noun3,
         number = number, adj2 = adj2, noun4 = noun4, noun5 = noun5, noun6 = noun6))
-madlibs()
+#print(selectStory(stories))
+# madlibs()
